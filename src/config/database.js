81189f74213c6a78227ea3928a0b391cba3ee84c
Felipe Strauss Ruffo <fesruffo@gmail.com>
2020-07-24 14:13:08 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,26 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'donamaid',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+    "development": {
+        dialect: 'postgres',
+        host: 'localhost',
+        username: 'postgres',
+        password: 'docker',
+        database: 'donamaid',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        },
+    },
+    "production": {
+        dialect: 'postgres',
+        host: 'ec2-54-197-254-117.compute-1.amazonaws.com',
+        username: 'pxqctadzmbjbtv',
+        password: '6b7b38b3f5fc9452859d46c903cb20a566e3fa5342f31ba0161365fc3863d190',
+        database: 'd9jk9jm7t3n2j4',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        },
+  }
 };
