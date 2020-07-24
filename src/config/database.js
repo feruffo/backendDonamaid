17217ac7b90f37ccf01e5f710e81508ac3007aeb
Felipe Strauss Ruffo <fesruffo@gmail.com>
2020-07-24 15:27:21 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "development": {
+    development: {
         dialect: 'postgres',
         host: 'localhost',
         username: 'postgres',
@@ -11,12 +11,13 @@ module.exports = {
             underscoredAll: true,
         },
     },
-    "production": {
+    production: {
         dialect: 'postgres',
         host: 'ec2-54-197-254-117.compute-1.amazonaws.com',
         username: 'pxqctadzmbjbtv',
         password: '6b7b38b3f5fc9452859d46c903cb20a566e3fa5342f31ba0161365fc3863d190',
         database: 'd9jk9jm7t3n2j4',
+        use_env_variable: 'DATABASE_URL',
         define: {
             timestamps: true,
             underscored: true,
