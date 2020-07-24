368ea6a46fe1de7c15153b79c22868cde663cdc6
Felipe Strauss Ruffo <fesruffo@gmail.com>
2020-07-24 16:07:06 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,12 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+//     this.connection = new Sequelize(databaseConfig);
+    this.connection = new Sequelize('d9jk9jm7t3n2j4', 'pxqctadzmbjbtv', '6b7b38b3f5fc9452859d46c903cb20a566e3fa5342f31ba0161365fc3863d190', {
+      host: 'ec2-54-197-254-117.compute-1.amazonaws.com',
+      dialect:'postgres'
+    });
+    
     models.map((model) => model.init(this.connection));
     models.map((model) => model.associate(this.connection.models));
   }
